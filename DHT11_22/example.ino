37daa352971722f46df37e6f0968d547cb17fe10
mcinm <78694399+mcinm@users.noreply.github.com>
2021-11-11 11:09:02 +0100

    example of arduino use

--- DHT11_22/example.ino
+++ DHT11_22/example.ino
@@ -1,14 +1,32 @@
 #include "DHT.h"
 
+unsigned char tab[5];
 
-void update_dht(void)
+
+// After this function data are ready to use
+void get_mes(void)
 {
   request();
   response();
   
+  tab[0] = get_data();
+  tab[1] = get_data();
+  tab[2] = get_data();
+  tab[3] = get_data();
+  tab[4] = get_data();
+  
+  unsigned char control = tab[0] + tab[1] + tab[2] + tab[3];
+  if (control != tab[4]) get_mes();
 }
 
+
 void setup()
+{
+
+}
+
+
+void loop()
 {
 
 }
